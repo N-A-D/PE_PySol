854c19b210a78d0cdcb78f2ee149e7b6dc2022ad
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-01-29 10:55:18 -0500

    Modified comments

--- solutions/functions.py
+++ solutions/functions.py
@@ -1,10 +1,9 @@
-# Computes the greatest common divisor
+# Computes the greatest common divisor between two numbers
 def gcd(x, y):
     while y != 0:
         x, y = y, x % y
     return x
 
-# Computes the lowest common divisor
+# Computes the lowest common multiple between two numbers
 def lcm(x, y):
     return x * y // gcd(x, y)
-    
